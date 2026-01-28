84c7c9dae939b953a54b0d08655f15ac1c5d5f16
katami_event <katami.jw+test@gmail.com>
2026-01-28 15:25:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.title("クラゲです…Streamlitで直接文字を表示しています<br>Renderでアプリを公開できましたね<br>これからRenderを使ったアプリの公開方法を解説していくのでみんなで頑張りましょう！")
+st.title("クラゲです…Streamlitで直接文字を表示しています Renderでアプリを公開できましたね これからRenderを使ったアプリの公開方法を解説していくのでみんなで頑張りましょう！")
